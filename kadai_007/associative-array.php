37b93503ff8d3273ba5acaa20218edd02e135c6e
yokoishikawa <yokkorobouro@gmail.com>
2026-06-14 07:03:27 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>課題007</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        // Step2. キーと値を持つ連想配列を作成し、変数に代入する
+        $onion_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+        // Step3. print_r関数を使って連想配列を出力する
+        print_r($onion_data);
+        ?>
+    </p>
+</body>
+
+</html>
